046d9aa1aeecab70fdeadedd0f7e2c390182e8c2
unknown <vishakvijayan2002@gmail.com>
2026-02-26 13:07:05 +0530

    asset issue fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        // Disable manual chunks to prevent loading issues
+        manualChunks: undefined
+      }
+    }
+  },
+  // Ensure dependencies are bundled together
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-redux', '@reduxjs/toolkit', 'react-router-dom', 'axios', 'react-bootstrap']
+  }
 })
