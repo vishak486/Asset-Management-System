24507a6683915e364b09e732003ac11e8495a98d
unknown <vishakvijayan2002@gmail.com>
2026-02-26 13:15:04 +0530

    downgrade react version to fix issue

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        // Disable manual chunks to prevent loading issues
-        manualChunks: undefined
-      }
-    }
-  },
-  // Ensure dependencies are bundled together
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-redux', '@reduxjs/toolkit', 'react-router-dom', 'axios', 'react-bootstrap']
+    minify: 'esbuild'
   }
 })
